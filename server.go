30713cb1d5d66d1bd66983aeb0ccbf9f27a04386
jawher.kallel <jawher.kallel@key-performance-group.com>
2024-07-29 16:50:32 +0100

    feature/resolve_cors_error

--- server.go
+++ server.go
@@ -5,8 +5,9 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/99designs/gqlgen/graphql/handler"
+    "github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gorilla/handlers"
 	"github.com/jawherkl/go-crud-graphql/graph"
 	"github.com/jawherkl/go-crud-graphql/graph/generated"
 	"github.com/joho/godotenv"
@@ -25,13 +26,23 @@ func main() {
 		log.Fatal("Error loading .env file")
 	}
 	
-	//srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	// Connexion à la base de données
 	Database := graph.Connect()
+
+    // Création du serveur GraphQL
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{DB: Database}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	// Configuration des en-têtes CORS
+    corsHandler := handlers.CORS(
+        handlers.AllowedOrigins([]string{"http://localhost:3000"}), // Autoriser les requêtes depuis ce domaine
+        handlers.AllowedMethods([]string{"GET", "POST", "OPTIONS"}),
+        handlers.AllowedHeaders([]string{"Content-Type", "Authorization"}),
+    )
 
-	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
-	log.Fatal(http.ListenAndServe(":"+port, nil))
+    // Configuration des routes
+    http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+    http.Handle("/query", corsHandler(srv))
+
+    log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
+    log.Fatal(http.ListenAndServe(":"+port, nil))
 }
